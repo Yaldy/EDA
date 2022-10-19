7eaac7ffc3607f3e27ffa4c7ae68af66556f8a2a
yaldy <natalia.vegaq@usm.cl>
2022-10-18 19:06:41 -0700

    Se cambia el nombre a stack_list_mio y el nuevo estack list es el del caqui

--- Taller2/stack_list.c
+++ Taller2/stack_list.c
@@ -1,95 +1,79 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include "stack.h"
 
-//#define STACK_MAX 20
-
-typedef struct stacknode{
-	int val;
-	struct stacknode *next;
+typedef struct stacknode
+{
+    int val;
+    struct stacknode *next;
 }STACK;
 
 static STACK *head = NULL;
 
-int nelem=0;
+void STACKclean()
+{
+    STACK *current = head;
+    STACK *next;
 
-int STACKsize(){
-	return(nelem);
+    while (current != NULL)
+    {
+       next = current->next;
+       free(current);
+       current = next;
+    }
+
+    head = NULL;
 }
 
-void STACKclean(){
-	int con=0;
-	STACK *aux=NULL;
-	//printf("%d\n",head->next);
-	if(STACKsize()>0){
-		while(head!=NULL){
-			con++;
-			printf("%d\n",con);
-			aux=head->next;
-			free(head);
-			head=aux;
-		}
-	}
-	
-	//printf("Stack eliminado.\n");	
+void STACKinit(N)
+{
+    if (head != NULL){
+        STACKclean();
+    } 
+        
 }
 
-
-void STACKinit(int size){
-	if(head!=NULL){
-		STACKclean(head);
-	}
-		
+int STACKempty()
+{
+    return(head==NULL);
 }
 
-int STACKempty(){
-	return(head==NULL);
+void STACKpush(int var, int MAX_SIZE)
+{
+    if ( STACKsize( head ) <= MAX_SIZE ){
+        STACK *newNode = malloc(sizeof(STACK));
+        newNode -> val = var;
+        newNode -> next = head;
+        head = newNode;
+    }
+    else{
+        printf("Error: el stack está lleno. No se añadió ningún valor.\n");
+    }
 }
 
-void STACKpush(int val,int max){
-	if(STACKsize()<max){
-		STACK* newNode =  malloc(sizeof(STACK));
-		newNode -> val = val;
-		newNode -> next = head;
-		head = newNode;
-		nelem++;
-	}
-	else{
-		printf("ERROR: no queda espacio en el stack. Dato no se guardo.\n");
-	}
+int STACKpop()
+{
+    STACK *temp;
+    if (STACKempty())
+        printf("Error: el stack está vacío.\n");
+    else {
+        int res = head -> val;
+        temp = head;
+        head = head -> next;
+        free(temp);
+        
+        return res;
+    }
+
 }
 
-int STACKpop(){
-	if(STACKsize()>0){
-		int res = head -> val;
-		STACK *temp=head;
-		head = head -> next;
-		free(temp);
-		nelem--;
-		return res;
-	}
-	else{
-		printf("ERROR: no hay más contenido en el stack\n");
-		return 0;
-	}
+int STACKsize(STACK *head)
+{
+    int largo = 0;
+    while ( head!= NULL ){
+        largo++;
+        head = head -> next;
+    }
+    return largo;
 }
-
-
-/*main para probar implementación
-void main(int argc, char** argv){
-	STACKinit(argc);
-	int dato;
-	int i = 1;
-	for (i;i<argc;i++){
-		dato = atoi(argv[i]);
-		//printf("dato es %d\n",dato);
-		STACKpush(dato,STACK_MAX);
-		printf("el dato %d se guardo en espacio %d del stack\n",dato,nelem-1);
-	}
-	
-	for (i=1;i<argc-2;i++){
-		printf("del stack sale %d\n", STACKpop());	
-	}
-	printf("se va a limpiar el stack\n");
-	STACKclean();
-}*/
